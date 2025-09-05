2663795905ea43030f7f03284b89aa7f9c5fabf8
anand8080-del <anandjagtap8080@gmail.com>
2025-09-05 11:11:07 +0530

    java core

---
+++ Datatypes.java
@@ -0,0 +1,44 @@
+package datatypes;
+
+public class Datatypes {
+public static void main(String args[])
+{
+	byte a=127;
+	//the formula of range calculation is -2^n-1 to 2^n-1-1
+	//the range of byte is -128 to 127
+	System.out.println(a);
+	// these formula is applied on all datatypes to calculate the range of datatypes
+	/**
+	1) byte = 1bit -it is used for only true or false 
+	        1 for true & 0 for false
+	        
+	2)short = 2byte = 16bits 
+	        the range is 32768 to 32767
+	        
+	3)int = 4byte = 32bits
+	the range is less than the long
+	
+	4)float =4byte =32bits
+	the range is less then double
+	
+	5)long =8byte =64bits
+	the range of long is more than int
+	
+	6)double =8bytes =64bits
+	the range is more than float
+	
+	7) char =2bytes =16bits
+	use for special chracters or single letter.
+	
+	//# null value not be a Accssed
+	        
+	 */
+	
+	
+
+
+
+
+
+}
+}
