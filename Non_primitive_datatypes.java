eb4ba74b051bf5fb3e3a350ba08266deedd7697f
anand8080-del <anandjagtap8080@gmail.com>
2025-09-05 11:23:37 +0530

    non premitive datatypes

---
+++ Non_primitive_datatypes.java
@@ -0,0 +1,60 @@
+package datatypes;
+
+public class Non_primitive_datatypes {
+
+	public static void main(String[] args) {
+		//1) Array
+	// Array is the collecton of datadypes
+		//Array is the lenear and homogeneous: when you have to assign the lenght then data assigned side by side or adjusent
+		//lenght count start from the 0
+		// 5 : length is five but count start with 0
+		//variable.length: .length give the length of an array
+		//null value accssed in all non primitive datatypes
+		
+		/*int [] salary=new int[4];
+		System.out.println(salary.length);
+		//NEW it helps to create new Array. it also known as object or instance
+		salary[0]=10000;
+		salary[1]=20000;
+		salary[2]=10000;
+		salary[3]=20000;
+		System.out.println("salary value is:"+ salary[3]);
+		//2) String
+		// string is the collection of letters or characters
+		//string is not perforn mathematical operations
+		*/
+		/*String name="hbdxesgdyuxgdwfdyuqwixsifdyi";
+		System.out.println(name.length());
+		String []str=new String[3];
+		str[0]="anand";
+		str[1]="samadhan";
+		str[2]="tejas";
+		System.out.println("Name is:"+str[0]);
+		System.out.println(str[0].length());
+		System.out.println(str[1].length());
+		
+		//these () is used in string for check length
+		*/
+		
+	/*	int [] anand=new int [5];
+		anand[0]=20000;
+		anand[1]=5655;
+		anand[2]=9878;
+		anand[3]=86765;
+		anand[4]=65444;
+		System.err.println(anand.length);
+		System.out.println("anand salary is"+anand[0]);
+		
+			*/
+		String SBPCOE="ajhjlkfhlrfhrfjbfjdkljfefblfjkfbrbrbej";
+		System.out.println(SBPCOE.length());
+				
+		
+		
+		
+		
+		
+
+	}
+
+}
