9446643f3e72058b4f411839f50a1a7c2207a94d
anand8080-del <anandjagtap8080@gmail.com>
2025-09-08 12:42:08 +0530

    assignment_operator

---
+++ Assignment_operator.java
@@ -0,0 +1,27 @@
+package java_codes;
+
+public class Assignment_operator {
+
+	public static void main(String[] args) {
+		//ASSIGNMENT OPERATOR 
+		//DEF: assigning the value and perform mathematical operations 
+		//=,+=,-=,%=,/=,*= these operators is known as assignment operators
+		//short hand property in assignment operators
+		// num+=3  example
+		
+		
+		
+		int raj = 10;
+		int ram =10000;
+		ram+=20000;
+		raj-=40;
+		System.out.println("total salary of ram and raj___"+ram);
+		System.out.println(raj+=120);
+		System.out.println(raj*=125);
+		System.out.println(ram/=2);
+		
+		
+
+	}
+
+}
