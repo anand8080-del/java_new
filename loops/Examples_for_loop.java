9e911bc91c809048b492280bf89efa2bd13261b9
anand8080-del <anandjagtap8080@gmail.com>
2025-09-16 12:06:12 +0530

    loops operation

---
+++ loops/Examples_for_loop.java
@@ -0,0 +1,31 @@
+package java_codes.loops;
+
+public class Examples_for_loop {
+
+	public static void main(String[] args) {
+		/**Q1) print square pattern like this ******
+                                              ******
+                                              ******
+                                              ******
+    
+    */
+		
+		for (int row=1;row<=4;row++)
+		{
+			System.out.println("****");
+			
+			
+		}
+		
+		for (int anuu=1;anuu<=10;anuu++)
+		{
+			System.out.println("heyy");
+		}
+	
+		
+		
+	}
+	
+	
+
+}
---
+++ loops/For_loop.java
@@ -0,0 +1,30 @@
+package java_codes.loops;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		//loops: one thing doing for specific condition or time 
+		//1) FOR LOOP
+		   // FOR(initialization;condition;opern/updation)
+		   // -initialization
+		    //-condition
+		   // -operation on variables
+		    //#ICBO 
+		    for (int i=10;i<20;i++)
+		    {
+		    	System.out.println("hii"); 
+		    	
+		    }
+		    for (int i=5;i>=1;i--)
+		    {
+		    	System.out.println("bye");
+		    }
+		    for (int i=5;i>=1;--i)
+		    	
+		    {
+		    	
+		    	System.out.println("anand");
+		    }
+	}
+
+}
