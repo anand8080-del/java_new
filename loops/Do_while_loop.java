d60d037372dd929910aa3a2d7ab3843c0c0f3d1f
anand8080-del <anandjagtap8080@gmail.com>
2025-09-17 19:37:29 +0530

    do while program

---
+++ loops/Do_while_loop.java
@@ -0,0 +1,19 @@
+package java_codes.loops;
+
+public class Do_while_loop {
+
+	public static void main(String[] args) {
+      
+		 int num=10;
+		 do {
+			 System.out.println(num);
+			 num++;
+		 
+		 }
+		 while (num<=10);
+		 {
+			System.out.println("anand"); 
+		 }
+	}
+
+}
